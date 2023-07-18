2a94ad0c7302cebfa59a77ea340ed25a34fd9818
mike-mbugua <mikembugua960@gmail.com>
2023-07-18 11:12:11 +0300

    add controller actions

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,34 @@
+class EventsController < ApplicationController
+    def index
+        event=Event.all
+        render json: event, status: :ok
+    end
+    def show
+        event=Event.find(params[:id])
+        render json:event
+    end
+    def create
+        event=Event.create(event_params)
+        render json: event, status: :created
+    end
+    def update
+        event=Event.find(params[:id])
+        event.update(event_params)
+        render json: event
+    end
+    def destroy
+        event=Event.find(params[:id])
+        event.destroy
+    end
+    
+    
+    
+    private
+
+    def event_params
+        params.permit(:name,:picture,:details,:date,:requirements,:organisers,:status)
+        
+    end
+    
+    
+end
